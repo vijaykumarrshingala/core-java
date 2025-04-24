1acb9ed60b3fbe2f2b8e547c0ebbd732077e6c3a
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-04-24 17:58:04 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(SslBundles sslBundles) {
+        SslBundle sslBundle = sslBundles.getBundle("mybundle");
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(
+                org.apache.hc.client5.http.impl.classic.HttpClients.custom()
+                        .setSSLContext(sslBundle.createSslContext())
+                        .build());
+        return new RestTemplate(requestFactory);
+    }
+}
