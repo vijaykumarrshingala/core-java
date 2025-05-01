f68e6f2a7494c2d9e54f8b6a862f9306fa52dedd
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-05-01 06:56:09 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,39 +1,9 @@
-import io.netty.handler.ssl.SslContextBuilder;
-import org.springframework.boot.ssl.SslBundle;
-import org.springframework.boot.ssl.SslBundles;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-@Configuration
-public class RestClientConfig {
-
-    private final SslBundles sslBundles;
-
-    public RestClientConfig(SslBundles sslBundles) {
-        this.sslBundles = sslBundles;
-    }
-
-    @Bean
-    public RestClient restClient() throws Exception {
-        SslBundle sslBundle = sslBundles.getBundle("client");
-
-        var sslContext = SslContextBuilder
-                .forClient()
-                .trustManager(sslBundle.getTrustManagerFactory())
-                .keyManager(sslBundle.getKeyManagerFactory())
-                .build();
-
-        HttpClient httpClient = HttpClient.create()
-                .secure(t -> t.sslContext(sslContext));
-
-        WebClient webClient = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
-
-        return RestClient.builder(webClient).build();
+public class ProxyTest {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("java.net.useSystemProxies", "true");
+        var url = new java.net.URL("https://www.google.com");
+        var conn = (java.net.HttpURLConnection) url.openConnection();
+        conn.connect();
+        System.out.println("Response: " + conn.getResponseCode());
     }
 }
