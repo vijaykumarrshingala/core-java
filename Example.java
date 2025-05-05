d1ac4032a3b84a13b38ebff12ba6c7b4321ce0ce
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-05-05 15:07:55 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,27 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.*;
 
-public class WindowsProxySettings {
+public class HttpGetUsingSystemProxy {
 
-    public static void main(String[] args) throws Exception {
-        // Ask Java to use system proxies
-        System.setProperty("java.net.useSystemProxies", "true");
+    public static void main(String[] args) {
+        try {
+            // Enable use of OS-level proxy settings
+            System.setProperty("java.net.useSystemProxies", "true");
 
-        URI uri = new URI("http://www.google.com"); // you can test with https as well
+            URI uri = new URI("http://www.google.com");
 
-        System.out.println("Detecting proxies for: " + uri);
+            System.out.println("Detecting proxy for: " + uri);
+            ProxySelector selector = ProxySelector.getDefault();
+            Proxy proxy = selector.select(uri).get(0);
 
-        ProxySelector selector = ProxySelector.getDefault();
-        for (Proxy proxy : selector.select(uri)) {
+            // Show proxy info
             System.out.println("Proxy Type: " + proxy.type());
-
-            SocketAddress address = proxy.address();
-            if (address != null) {
-                InetSocketAddress inetSocketAddress = (InetSocketAddress) address;
-                System.out.println("Proxy Host: " + inetSocketAddress.getHostName());
-                System.out.println("Proxy Port: " + inetSocketAddress.getPort());
+            if (proxy.address() != null) {
+                InetSocketAddress addr = (InetSocketAddress) proxy.address();
+                System.out.println("Using proxy: " + addr.getHostName() + ":" + addr.getPort());
             } else {
                 System.out.println("Direct connection (no proxy)");
             }
-        }
-    }
-}
+
+            // Open connection using the detected proxy
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection(proxy);
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000); // optional
+            connection.setReadTimeout(5000);    // optional
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream())
+            );
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine).append('\n');
+            }
+
+            in.close();
+            connection.disconnect();
