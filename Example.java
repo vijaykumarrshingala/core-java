792846c4207b64a3e5d7f207bd833f9c432bd7c9
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-05-05 13:11:27 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,9 +1,27 @@
-public class ProxyTest {
+import java.net.*;
+
+public class WindowsProxySettings {
+
     public static void main(String[] args) throws Exception {
+        // Ask Java to use system proxies
         System.setProperty("java.net.useSystemProxies", "true");
-        var url = new java.net.URL("https://www.google.com");
-        var conn = (java.net.HttpURLConnection) url.openConnection();
-        conn.connect();
-        System.out.println("Response: " + conn.getResponseCode());
+
+        URI uri = new URI("http://www.google.com"); // you can test with https as well
+
+        System.out.println("Detecting proxies for: " + uri);
+
+        ProxySelector selector = ProxySelector.getDefault();
+        for (Proxy proxy : selector.select(uri)) {
+            System.out.println("Proxy Type: " + proxy.type());
+
+            SocketAddress address = proxy.address();
+            if (address != null) {
+                InetSocketAddress inetSocketAddress = (InetSocketAddress) address;
+                System.out.println("Proxy Host: " + inetSocketAddress.getHostName());
+                System.out.println("Proxy Port: " + inetSocketAddress.getPort());
+            } else {
+                System.out.println("Direct connection (no proxy)");
+            }
+        }
     }
 }
