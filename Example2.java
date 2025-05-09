67f0d4592288d0e3ff975bf68a4d55c0cb9d639e
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-05-09 17:36:04 +0530

    Update Example2.java

--- Example2.java
+++ Example2.java
@@ -1,51 +1,54 @@
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.apache.hc.core5.http.HttpHost;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.util.Base64;
+import org.apache.hc.client5.http.config.RequestConfig;
 
-public class ProxyWithBasicAuthManual {
+public class ProxyWithAuthExample {
 
     public static void main(String[] args) throws Exception {
-        // Proxy details
-        String proxyHost = "abpm-di99-8001";
-        int proxyPort = 8085;
-        String username = "amit101";
-        String password = "yourPassword";
-
-        // Encode Basic Auth manually
-        String credentials = username + ":" + password;
-        String encodedCreds = Base64.encode(credentials.getBytes());
-        String proxyAuthHeader = "Basic " + encodedCreds;
+        // Proxy info
+        String proxyHost = "your.proxy.host";     // e.g., "10.10.1.100"
+        int proxyPort = 8080;
+        String proxyUser = "your_proxy_user";
+        String proxyPass = "your_proxy_password";
 
         // Target URL
-        String targetUrl = "https://httpbin.org/ip";
+        String targetUrl = "https://www.example.com";
 
-        // Create proxy host
-        HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+        // Set proxy
+        HttpHost proxy = new HttpHost("http", proxyHost, proxyPort);
 
-        // Build request config with proxy
-        RequestConfig config = RequestConfig.custom()
+        // Credentials provider for proxy
+        BasicCredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+            new AuthScope(proxy),
+            new UsernamePasswordCredentials(proxyUser, proxyPass.toCharArray())
+        );
+
+        // Build HTTP client
+        CloseableHttpClient httpclient = HttpClients.custom()
             .setProxy(proxy)
+            .setDefaultCredentialsProvider(credsProvider)
             .build();
 
-        // Build client
-        try (CloseableHttpClient client = HttpClients.custom()
-                .setDefaultRequestConfig(config)
-                .build()) {
+        try {
+            HttpGet httpget = new HttpGet(targetUrl);
+            System.out.println("Executing request: " + httpget.getMethod() + " " + httpget.getUri());
 
-            HttpGet request = new HttpGet(targetUrl);
+            httpclient.execute(httpget, response -> {
+                System.out.println("Response status: " + response.getCode());
+                System.out.println("Response body: " + EntityUtils.toString(response.getEntity()));
+                return null;
+            });
 
-            // Add Proxy-Authorization header manually
-            request.addHeader("Proxy-Authorization", proxyAuthHeader);
-
-            String response = client.execute(request, httpResponse ->
-                EntityUtils.toString(httpResponse.getEntity())
-            );
-
-            System.out.println("Response:\n" + response);
+        } finally {
+            httpclient.close();
         }
     }
 }
